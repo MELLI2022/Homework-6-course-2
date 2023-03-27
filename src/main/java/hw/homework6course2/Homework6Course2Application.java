c9763b079894d2161a3188dc75b41b10e350909d
MELLI2022 <denisenkoswetlana109@gmail.com>
2023-03-27 20:52:42 +0300

    Мой первый коммит.

---
+++ src/main/java/hw/homework6course2/Homework6Course2Application.java
@@ -0,0 +1,13 @@
+package hw.homework6course2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework6Course2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework6Course2Application.class, args);
+    }
+
+}
